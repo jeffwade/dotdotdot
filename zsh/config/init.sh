c692735ade6835ebdb04ec5e5273f77912d8040d
Jeff Wade <jeffwade@users.noreply.github.com>
2026-05-26 16:44:01 -0700

    add PATH and integrations to config/init.sh

--- zsh/config/init.sh
+++ zsh/config/init.sh
@@ -31,10 +31,38 @@ else
   source "$HOME/.zsh/spaceship/spaceship.zsh"
 fi
 
+########################################################################
+# ENVIRONMENT
+########################################################################
+
+# shell integrations
+test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+test -e "$HOME/.shellfishrc" && source "$HOME/.shellfishrc"
+
+# TODO: add function to source local files
+# source certificates
+source "$HOME/.certs"
+
+# bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+
+# PATH
+export PATH="/usr/local/sbin:$PATH"
+export PATH="$PATH:/usr/bin"
+export PATH="$PATH:$HOME/.local/bin"
+
+# activate jenv (java environment manager)
+# export PATH="$HOME/.jenv/bin:$PATH"
+# eval "$(jenv init -)"
+
 # EDITOR
 # export EDITOR="cursor --wait" #cursor
 # export EDITOR="code" #vscode
 export EDITOR="nvim" #neovim
+
+# bun completions
+[ -s "$HOME/.bun/_bun" ] && source "$HOME/.bun/_bun"
 
 # DO NOT TRACK
 export DO_NOT_TRACK=true
